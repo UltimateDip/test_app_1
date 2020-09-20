212505ed5fa43df3b8ea7ac4abb3911081d90bc0
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2020-09-20 21:02:55 +0530

    Delete login_screen.dart
    Not in use

--- lib/login_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoginScreen extends StatelessWidget {
-  static const id='login_screen';
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
